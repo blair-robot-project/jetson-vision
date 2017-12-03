b746950942105d33ca35e7ae6314d4e8512ddb4d
Noah Gleason <noah.gleason1@gmail.com>
2017-12-03 21:09:31 +0000

    Add settings script

---
+++ lifecamSettings.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+v4l2-ctl \
+--set-ctrl=brightness=30 \
+--set-ctrl=contrast=10 \
+--set-ctrl=saturation=200 \
+--set-ctrl=white_balance_temperature_auto=0 \
+--set-ctrl=power_line_frequency=0 \
+--set-ctrl=white_balance_temperature=2800 \
+--set-ctrl=sharpness=50 \
+--set-ctrl=backlight_compensation=0 \
+--set-ctrl=exposure_auto=1 \
+--set-ctrl=exposure_absolute=5 \
+--set-ctrl=pan_absolute=201600 \
+--set-ctrl=tilt_absolute=201600 \
+--set-ctrl=zoom_absolute=0 \
