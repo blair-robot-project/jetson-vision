cd99480970697f7a2ded46e089054677212f5339
Noah Gleason <noah.gleason1@gmail.com>
2019-01-21 12:52:46 -0500

    Don't install examples

--- installOpenCV.sh
+++ installOpenCV.sh
@@ -3,13 +3,13 @@ sudo apt update
 sudo apt -y upgrade
 
 sudo apt -y remove x264 libx264-dev
- 
+
 sudo apt -y install build-essential checkinstall cmake pkg-config yasm
 sudo apt -y install git gfortran
 sudo apt -y install libjpeg8-dev libjasper-dev libpng12-dev
 
 sudo apt -y install libtiff5-dev
- 
+
 sudo apt -y install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev
 sudo apt -y install libxine2-dev libv4l-dev
 sudo apt -y install libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
@@ -38,7 +38,7 @@ git checkout 3.4.5
 mkdir build
 cd build
 
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D WITH_CUDA=ON -D ENABLE_FAST_MATH=1 -D CUDA_FAST_MATH=1 -D WITH_CUBLAS=1 -D WITH_TBB=ON -D WITH_V4L=ON -D WITH_QT=ON -D WITH_OPENGL=ON -D PYTHON_EXECUTABLE=/usr/bin/python3 -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules -D BUILD_EXAMPLES=ON -D CUDA_NVCC_FLAGS="--expt-relaxed-constexpr" ..
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D INSTALL_C_EXAMPLES=OFF -D INSTALL_PYTHON_EXAMPLES=OFF -D WITH_CUDA=ON -D ENABLE_FAST_MATH=1 -D CUDA_FAST_MATH=1 -D WITH_CUBLAS=1 -D WITH_TBB=ON -D WITH_V4L=ON -D WITH_QT=ON -D WITH_OPENGL=ON -D PYTHON_EXECUTABLE=/usr/bin/python3 -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules -D BUILD_EXAMPLES=OFF -D CUDA_NVCC_FLAGS="--expt-relaxed-constexpr" ..
 make -j$(nproc)
 sudo make install
 sudo sh -c 'echo "/usr/local/lib" >> /etc/ld.so.conf.d/opencv.conf'
